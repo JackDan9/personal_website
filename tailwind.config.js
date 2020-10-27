7c1ba280d50e43d6fff9fd060ddb6786cf690958
JackDan9 <1835812864@qq.com>
2020-10-27 22:26:03 +0800

    Use tailwindcss
    1. Verify tailwindcss configuration;
    2. Refer to `main.ts`.
    
    Cloese-Bug: #personal-website-002

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,77 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  purge: ['./src/**/*.{js,mdx}'],
   important: true,
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
   theme: {
     screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-    },
-    fontFamily: {
-      display: ['Gilroy', 'sans-serif'],
-      body: ['Graphical', 'sans-serif'],
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1280px',
     },
     extend: {
-      colors: {
-        cyan: '#9cdbff',
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        source: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+        'ubuntu-mono': ['Ubuntu Mono', ...defaultTheme.fontFamily.mono],
+        system: defaultTheme.fontFamily.sans,
       },
-      margin: {
-        '96': '24rem',
+      colors: {
+        code: {
+          green: 'var(--color-code-green)',
+          yellow: 'var(--color-code-yellow)',
+          purple: 'var(--color-code-purple)',
+          red: 'var(--color-code-red)',
+          blue: 'var(--color-code-blue)',
+          white: 'var(--color-code-white)',
+        },
+        'green-150': '#e6ffee',
+      },
+      spacing: {
+        '2px': '2px',
+        '7': '1.75rem',
+        '10': '2.5rem',
+        '12': '3rem',
+        '16': '4rem',
+        '20': '5rem',
+        '24': '6rem',
+        '28': '7rem',
+        '32': '8rem',
+        '80': '20rem',
         '128': '32rem',
+        '(screen-16)': 'calc(100vh - 4rem)',
+      },
+      inset: {
+        '16': '4rem',
+        '20': '5rem',
+        '24': '6rem',
+      },
+      borderWidth: {
+        '6': '6px',
+      },
+      maxWidth: (theme) => ({
+        'screen-xl': theme('screens.xl'),
+      }),
+      maxHeight: {
+        xs: '20rem',
+        sm: '30rem',
+        '(screen-16)': 'calc(100vh - 4rem)',
+      },
+      boxShadow: {
+        'md-light': '0 0 12px 8px rgb(255,255,255)',
+      },
+      zIndex: {
+        '90': '90',
+        '100': '100',
       },
     },
   },
   variants: {
-    opacity: ['responsive', 'hover']
+    backgroundColor: ['responsive', 'odd', 'even', 'hover', 'focus'],
+    borderColor: ['responsive', 'hover', 'focus'],
+    borderWidth: ['responsive', 'first', 'last', 'hover', 'focus'],
+    textColor: ['responsive', 'group-focus', 'group-hover', 'hover', 'focus'],
+    opacity: ['responsive', 'hover', 'focus', 'disabled', 'group-hover'],
   },
-  plugins: [],
 }
