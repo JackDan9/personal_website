78ab92039520a611589129a57aa217d6ebb584de
jackdan <1835812864@qq.com>
2021-11-29 21:43:13 +0800

    Update `three number sum` maximum

--- src/views/blog-detail/markdown/2018/code/let-and-const/test.js
+++ src/views/blog-detail/markdown/2018/code/let-and-const/test.js
@@ -1,4 +1,13 @@
-// freeze:冻结
-const _a = Object.freeze({});
-_a.name="jackdan"; // TypeError: Cannot add property name, object is not extensible
-console.log(_a);
+function _max(nums) {
+  let _len = nums.length;
+  let _result = nums[0] + nums[1] + nums[2];
+
+  let i = 1;
+  while(i < _len - 2) {
+    _result = Math.max(nums[i] + nums[i + 1] + nums[i + 2], _result);
+    i++;
+  }
+  return _result;
+}
+
+
